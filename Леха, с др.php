fa33466cf19ac5993650e853e9f5f840d02c3b43
KIRkirKIR <34437220+KIRkirKIR@users.noreply.github.com>
2017-12-11 17:46:31 +0700

    Add files via upload

---
+++ Леха, с др.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+ <meta charset="UTF-8"/>
+ <title>Привет леха</title>
+ <style type="text/css">
+h6 {
+  font-size: 50px;
+  color: red;
+  background-color: black;
+  border: 3px solid red;
+  border-radius: 20px;
+  padding: 15px;
+}
+h5 {
+ font-size: 100px;
+ opacity:0;
+ color:black;
+}
+h5:hover {
+  opacity: 1;
+  font-size:100 px;
+}
+ </style>
+</head>
+<body>
+  <h6>
+   <div align="center">
+   С ДНЕМ РОЖДЕНИЯ ТЕБЯ, ЛЕХА ! Наведи мышку ниже что-бы посмотреть пожелания.
+   </div>
+  </h6>
+  <h5>
+    <div align="center">
+      Желаю, конечно же, счастья, здоровья, успехов.
+	  Так же продолжай свой путь воллейболиста.
+	  Зарабатывать много денег.
+	  И учись на одни пятерки.
+	</div>
+  </h5>
+</body>
+</html>
